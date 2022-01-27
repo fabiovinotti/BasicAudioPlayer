0b9f4afa01aa0b6832a12be3fd39a6cde3c57b0b
Fabio Vinotti <fabio@keenweb.io>
2022-01-28 00:43:43 +0100

    Add function to detach all attached nodes

--- Sources/BasicAudioPlayer/AudioNodes/AVAudioEngine+Extensions.swift
+++ Sources/BasicAudioPlayer/AudioNodes/AVAudioEngine+Extensions.swift
@@ -12,4 +12,10 @@ extension AVAudioEngine {
     public func attach(_ nodes: Set<AVAudioNode>) {
         for n in nodes { attach(n) }
     }
+    
+    public func detachAll() {
+        for n in attachedNodes {
+            detach(n)
+        }
+    }
 }
