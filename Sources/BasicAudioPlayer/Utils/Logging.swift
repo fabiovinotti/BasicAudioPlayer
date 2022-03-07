87a9c49bab4d5c4396c2fc217ab96321dec3bbfe
Fabio Vinotti <hello@fabiovinotti.com>
2022-03-07 11:53:17 +0100

    Add logging utility documentation

--- Sources/BasicAudioPlayer/Utils/Logging.swift
+++ Sources/BasicAudioPlayer/Utils/Logging.swift
@@ -10,6 +10,10 @@ import os
 
 fileprivate let moduleLogger = Logger(subsystem: "BasicAudioPlayer", category: "general")
 
+/// Logs a message of the specified type.
+///
+/// The "file", "function" and "line" parameters are automatically collected when the function is called.
+/// Don't provide any value for those.
 func log(level: OSLogType,
          _ message: String,
          file: String = #file,
