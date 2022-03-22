77707973aba10d59953b085e07f61dc684a24875
Fabio Vinotti <hello@fabiovinotti.com>
2022-03-23 00:16:45 +0100

    Remove deprecated methods

--- Sources/BasicAudioPlayer/AudioNodes/AVAudioPlayerNode+Extensions.swift
+++ Sources/BasicAudioPlayer/AudioNodes/AVAudioPlayerNode+Extensions.swift
@@ -20,33 +20,4 @@ extension AVAudioPlayerNode {
         return playerTime
     }
     
-    public func scheduleFile(_ file: AVAudioFile, at when: AVAudioTime?) -> AnyPublisher<Void, Never> {
-        Future<Void, Never> { promise in
-            self.scheduleFile(file, at: when) {
-                promise(.success(()))
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    public func scheduleSegment(_ file: AVAudioFile, startingFrame startFrame: AVAudioFramePosition, frameCount numberFrames: AVAudioFrameCount, at when: AVAudioTime?) -> AnyPublisher<Void, Never> {
-        
-        Future<Void, Never> { promise in
-            self.scheduleSegment(file, startingFrame: startFrame, frameCount: numberFrames, at: when) {
-                promise(.success(()))
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    
-    public func scheduleBuffer(_ buffer: AVAudioPCMBuffer, at when: AVAudioTime?, options: AVAudioPlayerNodeBufferOptions = []) -> AnyPublisher<Void, Never> {
-        
-        Future<Void, Never> { promise in
-            self.scheduleBuffer(buffer, at: when, options: options) {
-                promise(.success(()))
-            }
-        }
-        .eraseToAnyPublisher()
-    }
-    
 }
