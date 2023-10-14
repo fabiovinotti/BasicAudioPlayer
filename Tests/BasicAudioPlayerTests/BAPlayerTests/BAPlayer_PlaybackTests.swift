a8386a590623dd934b1b9f75d517a1c5f333b67e
Fabio Vinotti <hello@fabiovinotti.com>
2023-10-15 00:07:40 +0200

    Add test case

--- Tests/BasicAudioPlayerTests/BAPlayerTests/BAPlayer_PlaybackTests.swift
+++ Tests/BasicAudioPlayerTests/BAPlayerTests/BAPlayer_PlaybackTests.swift
@@ -11,53 +11,53 @@ import AVFoundation
 @testable import BasicAudioPlayer
 
 class BAPlayer_PlaybackTests: XCTestCase {
-    
+
     // MARK: - Test Case Setup
-    
+
     private static var audioFile: AVAudioFile!
-    
+
     static override func setUp() {
-        guard let fileURL = Bundle.module.url(forResource: "audio-sample", withExtension: "aac")
-        else { fatalError("Failed to retrieve audio file URL.") }
-                
+        guard let fileURL = Bundle.module.url(forResource: "audio-sample", withExtension: "aac") else { fatalError("Failed to retrieve audio file URL.")
+        }
+
         do {
             audioFile = try AVAudioFile(forReading: fileURL)
         } catch {
             fatalError("Failed to create audio file: \(error.localizedDescription)")
         }
     }
-    
+
     static override func tearDown() {
         audioFile = nil
     }
-    
+
     // MARK: - Single Test Setup
-    
+
     private var player: BAPlayer!
-    
+
     override func setUpWithError() throws {
         player = BAPlayer()
     }
-    
+
     override func tearDownWithError() throws {
         player = nil
     }
-    
+
     // MARK: - Test Play
-    
+
     func testPlay_BeforeLoading() {
         player.play()
-        
+
         XCTAssertEqual(player.status, .noSource)
         XCTAssertFalse(player.engine.isRunning)
     }
-    
+
     func testPlay_WhenStopped() {
         player.load(file: Self.audioFile)
         player.play()
         runPlayAssertions()
     }
-    
+
     func testPlay_WhenPaused() {
         player.load(file: Self.audioFile)
         player.play()
@@ -65,52 +65,58 @@ class BAPlayer_PlaybackTests: XCTestCase {
         player.play()
         runPlayAssertions()
     }
-    
+
     private func runPlayAssertions() {
         XCTAssertEqual(player.status, .playing)
         XCTAssert(player.engine.isRunning)
     }
-    
+
     // MARK: - Test Pause
-    
+
     func testPause_BeforeLoading() {
         player.pause()
         XCTAssertFalse(player.engine.isRunning)
         XCTAssertEqual(player.status, .noSource)
     }
-    
+
     func testPause_WhenPlaying() {
         player.load(file: Self.audioFile)
         player.play()
         player.pause()
-        
+
         XCTAssertFalse(player.engine.isRunning)
         XCTAssertEqual(player.status, .paused)
     }
-    
+
     func testPause_WhenStopped() {
         player.load(file: Self.audioFile)
         player.pause()
-        
+
         XCTAssertFalse(player.engine.isRunning)
         XCTAssertEqual(player.status, .ready)
     }
-    
+
     // MARK: - Test Stop
-    
+
     func testStop_BeforeLoading() {
         player.stop()
         XCTAssertEqual(player.status, .noSource)
         XCTAssertFalse(player.engine.isRunning)
     }
-    
+
+    func testStop_AfterLoading() {
+        player.load(file: Self.audioFile)
+        player.stop()
+        checksForStop()
+    }
+
     func testStop_WhenPlaying() {
         player.load(file: Self.audioFile)
         player.play()
         player.stop()
         checksForStop()
     }
-    
+
     func testStop_WhenPaused() {
         player.load(file: Self.audioFile)
         player.play()
@@ -118,9 +124,10 @@ class BAPlayer_PlaybackTests: XCTestCase {
         player.stop()
         checksForStop()
     }
-    
+
     private func checksForStop() {
         XCTAssertEqual(player.status, .ready)
         XCTAssertFalse(player.engine.isRunning)
+        XCTAssertNotNil(player.file)
     }
 }
